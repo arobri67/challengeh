e5f3e9ce561a4e1fbcbb891ead09e9d879a403a8
Arnaud <arnaud.obri@proton.me>
2025-09-20 16:24:55 +0200

    add relative date in card list

--- apps/mobile/lib/utils.ts
+++ apps/mobile/lib/utils.ts
@@ -1,6 +1,7 @@
 import { clsx, type ClassValue } from 'clsx';
 import { twMerge } from 'tailwind-merge';
 import { Platform } from 'react-native';
+import { formatDistanceToNow, parseISO } from 'date-fns';
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
@@ -13,3 +14,12 @@ export const WS_URL =
   Platform.OS === 'android'
     ? 'ws://10.0.2.2:8080/notifications'
     : 'ws://localhost:8080/notifications';
+
+export const formatRelativeDate = (dateString: string): string => {
+  try {
+    const date = parseISO(dateString); // Parse ISO string
+    return formatDistanceToNow(date, { addSuffix: true });
+  } catch (error) {
+    return 'Invalid date';
+  }
+};
