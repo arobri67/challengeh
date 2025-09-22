cffb6e519f1f210858b9a539475bc5fa6027ad5c
Arnaud <141914477+arobri67@users.noreply.github.com>
2025-09-22 12:33:22 +0200

    fix version validation (#14)

--- apps/mobile/components/documents/document-add-form.tsx
+++ apps/mobile/components/documents/document-add-form.tsx
@@ -1,12 +1,13 @@
+import { zodResolver } from '@hookform/resolvers/zod';
+import { Controller, useForm } from 'react-hook-form';
 import { Alert, View } from 'react-native';
-import { Input } from '../ui/input';
-import { Text } from '../ui/text';
-import { Button } from '../ui/button';
+
 import { useCreateDocument } from '@/hooks/use-create-doc';
 import { CreateDocumentFormData, createDocumentSchema } from '@/schema/document-schema';
 
-import { useForm, Controller } from 'react-hook-form';
-import { zodResolver } from '@hookform/resolvers/zod';
+import { Button } from '../ui/button';
+import { Input } from '../ui/input';
+import { Text } from '../ui/text';
 
 interface Props {
   onSuccess?: () => void;
@@ -46,12 +47,12 @@ export default function DocumentAddForm({ onSuccess }: Props) {
     );
     return;
   };
+
   const onSubmit = async (data: CreateDocumentFormData) => {
     try {
       await createDocumentMutation.mutateAsync(data);
       reset();
       onSuccess?.();
-      Alert.alert('Success', 'Document created successfully!');
     } catch (error) {
       Alert.alert('Error', 'Failed to create document');
     }
@@ -116,21 +117,6 @@ export default function DocumentAddForm({ onSuccess }: Props) {
           </Text>
         </Button>
       </View>
-      {/* <Text className="mb-1 text-sm font-semibold">Name</Text>
-      <Input className="mb-3" placeholder="name" />
-      <Text className="mb-1 text-sm font-semibold">Version</Text>
-      <Input className="mb-3" placeholder="version" />
-      <Text className="mb-1 text-sm font-semibold">File</Text>
-      <View className="flex-row">
-        <Button variant="outline" className="bg-white">
-          <Text className="font-semibold text-blue-500">Choose File</Text>
-        </Button>
-      </View>
-      <View className="mt-auto flex-row gap-3">
-        <Button className="flex-1 bg-blue-500">
-          <Text className="font-semibold">Submit</Text>
-        </Button>
-      </View> */}
     </>
   );
 }
